1013cf259a4eb9ab0f794908ae59dbcfb8813cfe
ivanrazak <168300142+IvanRazak@users.noreply.github.com>
2024-12-01 19:41:52 -0300

    Update KanbanCard.jsx

--- src/components/kanban/KanbanCard.jsx
+++ src/components/kanban/KanbanCard.jsx
@@ -32,7 +32,7 @@ const KanbanCard = ({ item, index, onClick }) => {
         >
           <div className="flex flex-col gap-2">
             <div className="flex justify-between items-start">
-              <p className="font-semibold">Pedido #{item.order_number}
+              <p className="font-semibold">#{item.order_number}
               <p className="text-sm text-gray-600 dark:text-gray-300">
               {item.customer?.name}
             </p>
